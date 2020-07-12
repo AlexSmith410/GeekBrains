82fd623ed31411c748a833da8afeee36d1e623fd
AlexSmith410 <alexsmith410@yandex.ru>
2020-07-12 13:28:58 +0300

    Added lesson 11

--- src/main/java/com/geekbrains/java/lesson11/TestRunner.java
+++ src/main/java/com/geekbrains/java/lesson11/TestRunner.java
@@ -1,17 +1,55 @@
 package com.geekbrains.java.lesson11;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class TestRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Class c = Tests.class;
-//        Method[] methods = c.getDeclaredMethods();
         runTests(c);
     }
 
-    public static void runTests(Class c){
-        if (!c.isAnnotationPresent(Test.class)) {
-            throw new RuntimeException("Unable to create table for class " + c.getSimpleName());
+    public static void runTests(Class c) throws Exception {
+        Method[] methods = c.getDeclaredMethods();
+        List<Method> testMethods = new ArrayList<Method>();
+        List<Method> initMethod = new ArrayList<Method>();
+        List<Method> endMethod = new ArrayList<Method>();
+        for (int i = 0; i < methods.length; i++) {
+            if ((methods[i].isAnnotationPresent(Test.class))) {
+                testMethods.add(methods[i]);
+            }
+            if ((methods[i].isAnnotationPresent(BeforeSuit.class))) {
+                initMethod.add(methods[i]);
+            }
+            if ((methods[i].isAnnotationPresent(AfterSuit.class))) {
+                endMethod.add(methods[i]);
+            }
         }
+        if (endMethod.size() > 1 || initMethod.size() > 1)
+            throw new RuntimeException();
+
+        if (initMethod.size() != 0)
+            System.out.println(initMethod.get(0).getName());
+
+        Collections.sort(testMethods, (o1, o2) -> {
+            if (o1.getAnnotation(Test.class).value() > o2.getAnnotation(Test.class).value()) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+
+        for (Method m : testMethods
+        ) {
+            System.out.println(m.getName());
+        }
+
+        if (endMethod.size() != 0)
+            System.out.println(endMethod.get(0).getName());
     }
 }
--- src/main/java/com/geekbrains/java/lesson11/Tests.java
+++ src/main/java/com/geekbrains/java/lesson11/Tests.java
@@ -2,23 +2,30 @@ package com.geekbrains.java.lesson11;
 
 public class Tests {
     @Test(3)
-    public void test1(){}
+    public void test1() {
+    }
 
     @Test(5)
-    public void test2(){}
+    public void test2() {
+    }
 
     @Test(1)
-    public void test3(){}
+    public void test3() {
+    }
 
     @Test(7)
-    public void test4(){}
+    public void test4() {
+    }
 
     @Test(4)
-    public void test5(){}
+    public void test5() {
+    }
 
     @BeforeSuit
-    public void init(){}
+    public void init() {
+    }
 
     @AfterSuit
-    public void end(){}
+    public void end() {
+    }
 }
